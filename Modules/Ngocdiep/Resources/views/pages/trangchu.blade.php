d8a217e7e4ebc2d4a69965e80e0a756447e32295
HienGratinx <hien.kbhbt@gmail.com>
2019-02-28 10:29:09 +0700

    11

--- Modules/Ngocdiep/Resources/views/pages/trangchu.blade.php
+++ Modules/Ngocdiep/Resources/views/pages/trangchu.blade.php
@@ -15,7 +15,7 @@
     <div id="homepage" class="page-body">
         <section class="homepage-category">
             <div class="uk-container uk-container-center">
-                <div class="uk-slidenav-position slider" data-uk-slider="{autoplay: true, autoplayInterval: 10500}">
+                <div class="uk-slidenav-position slider" data-uk-slider="{autoplay: false, autoplayInterval: 10500}">
                     <div class="uk-slider-container">
                         <ul class="uk-slider uk-grid uk-grid-small uk-grid-width-small-1-2 uk-grid-width-medium-1-3 uk-grid-width-large-1-4 list">
 
